cb5392ea46c92e19b3d0f66361ff765213ab4fba
vylu-18 <91791546+vylu-18@users.noreply.github.com>
2022-09-30 21:45:09 +0530

    Updated for loop with new algos

--- forloop.py
+++ forloop.py
@@ -1,2 +1,5 @@
 for i in range(10):
     print("Satwika Sri")
+
+print("For loops completed")
+print("Wait what about while loops")
--- while.py
+++ while.py
@@ -1,3 +1,4 @@
 a = 2
 while (2):
     print("Hi Satwika Sri")
+print("While loops completed")
